e9ccaa9121f5119b56dd252b30fb1793e7e40696
chaiwat8612 <89202968+chaiwat8612@users.noreply.github.com>
2022-03-02 23:25:26 +0700

    Add Person model and custom example

---
+++ TestList/Models/PersonModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TestList.Models
+{
+    public class PersonModel
+    {
+        public int PId { get; set; }
+        public string PName { get; set; }
+    }
+}
--- TestList/Program.cs
+++ TestList/Program.cs
@@ -1,16 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using TestList.Models;
 
 namespace TestList
 {
     class Program
     {
+        //static void Main(string[] args)
+        //{
+        //    var list = new List<int>() { 0, 1, 700, 0 };
+
+        //    // Find index of element 100 or greater.
+        //    int result = list.FindIndex(element => element >= 100);
+        //    Console.WriteLine("FINDINDEX: {0}", result);
+        //    Console.ReadKey();
+        //}
+
         static void Main(string[] args)
         {
-            var list = new List<int>() { 0, 1, 700, 0 };
+            var personList = new List<PersonModel>();
+            var personModel = new PersonModel();
+
+            //first person data
+            personModel.PId = 111;
+            personModel.PName = "JOHN";
+
+            personList.Add(personModel);
+
+            //second person data
+            personModel = new PersonModel();
+            personModel.PId = 222;
+            personModel.PName = "Sara";
+
+            personList.Add(personModel);
+
+            //third person data
+            personModel = new PersonModel();
+            personModel.PId = 333;
+            personModel.PName = "Gale";
+
+            personList.Add(personModel);
 
             // Find index of element 100 or greater.
-            int result = list.FindIndex(element => element >= 100);
+            int result = personList.FindIndex(person => person.PName == "Sara");
             Console.WriteLine("FINDINDEX: {0}", result);
             Console.ReadKey();
         }
